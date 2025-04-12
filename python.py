1cd040f921685b1a3e762ebb08f641f652f27e27
Elly Woo <wjwj0210@gmail.com>
2025-04-13 10:58:55 +1200

    Initial commit
    :wp

---
+++ python.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+temp = np.array([12,23,23,34,13,17,20])
+
+#print average temp for week
+ave_temp = np.mean(temp)
+print(ave_temp)
+
+#finding the maximun temp
+max_temp = np.max(temp)
+print(max_temp)
+
+#finding the minimum temp
+min_temp = np.min(temp)
+print(min_temp)
+
+#Convert Fahrenheit 
+fah_temp = temp * 9/5 + 32
+print(fah_temp)
+
+#Above 20 temp
+nrew_temp =  np.where(temp > 20)
+print(nrew_temp)
+
+
