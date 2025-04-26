f8dad8d4330929507fdcffa51064156d44defd61
Elly Woo <wjwj0210@gmail.com>
2025-04-27 09:25:58 +1200

    class updated

---
+++ 27042025/Factorial_class.py
@@ -0,0 +1,16 @@
+class Factorial:
+    
+    def factorial(self):
+   
+        number = int(input("What's the number?"))
+        fac = 1
+        for i in range(1 , number+1):
+            fac *= i
+            print(f"{i} factorial is {fac}")
+
+
+fact = Factorial()
+fact.factorial()
+
+
+    
---
+++ 27042025/attendence.py
@@ -0,0 +1,25 @@
+#Enter students into list
+
+
+#GetStudentInfo(name, studentId)
+#
+#class:
+#Student
+#camera
+#check
+
+
+
+
+def students():
+    
+    student = []
+    while 1:
+        student = input("Student's name: ")
+
+# Enter the student name into list by using loop
+def attend(num):
+    for _ in num:
+        print()
+
+students()
