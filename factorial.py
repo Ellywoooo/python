4f05efa9eeef329dc5c74b285a27b533df2b0ac1
Elly Woo <wjwj0210@gmail.com>
2025-04-26 12:55:59 +1200

    updated

--- factorial.py
+++ factorial.py
@@ -1,7 +1,7 @@
 def factorial(x, fac):
     for i in range(1 , x+1):
         fac *= i
-        print(f"{i} factorila is {fac}")
+        print(f"{i} factorial is {fac}")
 
 x = int(input("What's the number? "))
 fac = 1
