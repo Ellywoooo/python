ed46376e98960d25e3f67ac56ca1479037a43392
Elly Woo <wjwj0210@gmail.com>
2025-05-03 12:44:22 +1200

    Week4

--- Week4/File_database.py
+++ Week4/File_database.py
@@ -1,4 +1,4 @@
-import pandas as pd
-
-df = pd.read_csv('sample_junk_mail.csv')
-print(df.head(2))
+import tensorflow as tf
+(x_train,y_train),(x_test,y_test)= tf.keras.datasets.cifar10.load_data()
+print("Training data shape",x_train.shape, y_train.shape)
+ 
