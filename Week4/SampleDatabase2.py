0c32ab7235c0985abda3d2efcf31fa327e8ecf1c
Elly Woo <wjwj0210@gmail.com>
2025-05-04 08:43:49 +1200

    fixed

--- Week4/SampleDatabase2.py
+++ Week4/SampleDatabase2.py
@@ -3,5 +3,5 @@ import pandas as pd
 # Using pyarrow backend to read parquet
 df = pd.read_parquet("Sample_data_2.parquet")
 
-# Display all  
-print(df)
+# Display legnth of database
+print(len(df))
