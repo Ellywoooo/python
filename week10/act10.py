e02889e4d2e3bdfc0a55c9adbf93bcb921533d5a
Elly Woo <wjwj0210@gmail.com>
2025-06-14 12:32:43 +1200

    week10 code

--- week10/act10.py
+++ week10/act10.py
@@ -10,13 +10,14 @@ def main():
  
 def log_event(event_type, user_id, message):
     """Logs an event to a file with timestamp, event type, user ID, and message."""
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_message = f"[{timestamp}] [{event_type}] User: {user_id} - {message}\n"
+    
+    log_message = create_log_message(event_type, user_id, message)
     read_log_message(log_message)
+
+def create_log_message(event_type, user_id, message):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"[{timestamp}] [{event_type}] User: {user_id} - {message}\n"
 
 def read_log_message(log_message):
     with open("rental_log.txt", "a") as log_file:
         log_file.write(log_message)
-
-if __name__ == "__main__":
-    main()
