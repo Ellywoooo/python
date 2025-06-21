7debd134e7e78257b6db212b898336183dfbbb26
Elly Woo <wjwj0210@gmail.com>
2025-06-22 08:55:34 +1200

    week11 unittest

---
+++ week11/test.py
@@ -0,0 +1,22 @@
+import unittest
+
+def add(x,y):
+    return x + y
+
+
+'''
+This is a nuit test of the add function.
+It checks if the result of add(x,y) matches the expected value.
+'''
+class TestMathOperations(unittest.TestCase):
+    def test_add(self):
+        # add(2,3) = 5 and expected value is 5
+        self.assertEqual(add(2, 3), 5)
+        # add(-1,1) = 0 and expected value is 0
+        self.assertEqual(add(-1, 1), 0)
+
+        # add(2,-1) = 1 and expected value is 4
+        self.assertEqual(add(2, -1), 4)       
+
+if __name__ == "__main__":
+    unittest.main()
