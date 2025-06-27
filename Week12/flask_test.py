22907d44038a98ce03f34a0b01c80423b7eabf43
Elly Woo <wjwj0210@gmail.com>
2025-06-28 11:23:57 +1200

    changed p to header

--- Week12/flask_test.py
+++ Week12/flask_test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_flask():
-    return "<p>Hello, Flask!</p>"
+    return "<h1>Hello, Flask!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
