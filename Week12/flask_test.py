92df239a62b557aa4eb2d019c3990b2fb059725a
Elly Woo <wjwj0210@gmail.com>
2025-06-28 11:33:38 +1200

    flask

--- Week12/flask_test.py
+++ Week12/flask_test.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello_flask():
     return "<h1>Hello, Flask!</h1>"
 
+@app.route("/bye")
+def bye():
+    return "<h1>Goodbye, Flask!</h1>"
+
+@app.route("/username/<name>")
+def learn(name):
+    return f"<h1>{name} is learning Flask!</h1>"
+
 if __name__ == "__main__":
     app.run(debug=True)
